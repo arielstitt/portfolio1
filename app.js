83197be6e845bd04bf5f5e8115813b8962b670a3
arielstitt <arielstitt@gmail.com>
2018-05-21 15:57:47 -0400

    added onclick and animate to portfolio divs

--- app.js
+++ app.js
@@ -23,11 +23,57 @@ $(() => {
         }  // End if
     });
 
-    $('div.portfolio-item-1').click(function(){
+
+    
+    const $portfolioItem1 = $('.portfolio-item-1').click(function(){
         console.log("you clicked a thing")
-        //hide
+        //hide the other elements 
+        $('.portfolio-item-2, .portfolio-item-3, .portfolio-item-4').hide()
+        $('.portfolio-item-1').animate({
+            width: "80vw",
+            height: '80vh',
+          }, 1000, function() {
+            
+          });
+    })
+
+    const $portfolioItem2 = $('.portfolio-item-2').click(function(){
+        console.log("you clicked a thing")
+        //hide the other elements 
+        $('.portfolio-item-1, .portfolio-item-3, .portfolio-item-4').hide()
+        $('.portfolio-item-2').animate({
+            width: "80vw",
+            height: '80vh',
+            
+          }, 1000, function() {
+            
+          });
+    })
+
+    const $portfolioItem3 = $('.portfolio-item-3').click(function(){
+        console.log("you clicked a thing")
+        //hide the other elements 
+        $('.portfolio-item-1, .portfolio-item-2, .portfolio-item-4').hide()
+        $('.portfolio-item-3').animate({
+            width: "80vw",
+            height: '80vh',
+            
+          }, 1000, function() {
+            
+          });
     })
     
-    
+    const $portfolioItem4 = $('.portfolio-item-4').click(function(){
+        console.log("you clicked a thing")
+        //hide the other elements 
+        $('.portfolio-item-1, .portfolio-item-2, .portfolio-item-3').hide()
+        $('.portfolio-item-4').animate({
+            width: "80vw",
+            height: '80vh',
+            
+          }, 1000, function() {
+            
+          });
+    })
 
 });
