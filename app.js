e86c96cfab32477a90f3d8d401f668cc71019964
arielstitt <arielstitt@gmail.com>
2018-05-21 10:15:10 -0400

    attempted an overlay and screwed it up

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ $(() => {
     console.log("Everything is ready, let's do this");
 
     // ===============PORTFOLIO==============
-    $(".portfolio-item").click(function(){
-        $(".portfolio-item").animate({'height': '100vh', 'width': '100vw'});
-    });
     
+    openContent = () => {
+        $('.portfolio-item-1').style.display = 'block';
+    }
 });
