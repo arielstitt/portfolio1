10a2ce4bc53d8eca86f82cece2bea464ca8985d5
arielstitt <arielstitt@gmail.com>
2018-05-22 14:16:52 -0400

    added contact item and about box class of grow and shrink

--- app.js
+++ app.js
@@ -1,47 +1,87 @@
 $(() => {
     console.log("Everything is ready, let's do this");
 
-    // Add smooth scrolling on all links inside the navbar
     $("#myNavbar a").on('click', function (event) {
-        // Make sure this.hash has a value before overriding default behavior
         if (this.hash !== "") {
-            // Prevent default anchor click behavior
             event.preventDefault();
-            // Store hash
             var hash = this.hash;
-            // Using jQuery's animate() method to add smooth page scroll
-            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
             $('html, body').animate({
                 scrollTop: $(hash).offset().top
             }, 800, function () {
-            // Add hash (#) to URL when done scrolling (default click behavior)
                 window.location.hash = hash;
             });
-        }  // End if
+        }
     });
-// ======================PORTFOLIO ===============================
+    // ======================PORTFOLIO ===============================
     $('.portfolio-item-1').hover(
-        function(){
+        function () {
             $(this).addClass('grow')
             $('.portfolio-item-2, .portfolio-item-3, .portfolio-item-4').addClass('shrink')
         }
     )
     $('.portfolio-item-2').hover(
-        function(){
+        function () {
             $(this).addClass('grow')
             $('.portfolio-item-1, .portfolio-item-3, .portfolio-item-4').addClass('shrink')
         }
     )
     $('.portfolio-item-3').hover(
-        function(){
+        function () {
             $(this).addClass('grow')
             $('.portfolio-item-1, .portfolio-item-2, .portfolio-item-4').addClass('shrink')
         }
     )
     $('.portfolio-item-4').hover(
-        function(){
+        function () {
             $(this).addClass('grow')
             $('.portfolio-item-1, .portfolio-item-2, .portfolio-item-3').addClass('shrink')
+        }
+    )
+    // =====================ABOUT======================
+
+    $('.about-item-1').hover(
+        function () {
+            $(this).addClass('grow')
+            $('.about-item-2, .about-item-3, .about-item-4').addClass('shrink')
+        }
+    )
+    $('.about-item-2').hover(
+        function () {
+            $(this).addClass('grow')
+            $('.about-item-1, .about-item-3, .about-item-4').addClass('shrink')
+        }
+    )
+    $('.about-item-3').hover(
+        function () {
+            $(this).addClass('grow')
+            $('.about-item-1, .about-item-2, .about-item-4').addClass('shrink')
+        }
+    )
+    $('.about-item-4').hover(
+        function () {
+            $(this).addClass('grow')
+            $('.about-item-1, .about-item-2, .about-item-3').addClass('shrink')
+        }
+    )
+    // ================ CONTACT =================
+
+    $('.contact-item-1').hover(
+        function () {
+            $(this).addClass('grow')
+            $('.contact-item-2, .contact-item-3').addClass('shrink')
+        }
+    )
+
+    $('.contact-item-2').hover(
+        function () {
+            $(this).addClass('grow')
+            $('.contact-item-1, .contact-item-3').addClass('shrink')
+        }
+    )
+    $('.contact-item-3').hover(
+        function () {
+            $(this).addClass('grow')
+            $('.contact-item-2, .contact-item-1').addClass('shrink')
         }
     )
 });
